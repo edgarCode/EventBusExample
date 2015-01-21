c8feca7b510398ddd432525583bf844fb07cfa38
edgarCode <fy861116@126.com>
2015-01-21 18:03:42 +0800

    add user-sdk element

--- src/com/example/eventbus/MainActivity.java
+++ src/com/example/eventbus/MainActivity.java
@@ -12,8 +12,7 @@ public class MainActivity extends Activity implements OnClickListener {
     private TextView mTVLabel;
     private Button mBtnPostEvent;
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         mTVLabel = (TextView) findViewById(R.id.tv);
